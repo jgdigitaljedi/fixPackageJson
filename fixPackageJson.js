927d64c71979f1bda8cc91379901edb06fad72a9
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-03-29 14:27:52 -0500

    added bower and list of missed folders

--- fixPackageJson.js
+++ fixPackageJson.js
@@ -6,8 +6,9 @@
 var fs = require('fs');
 
 (function main () {
-    var results = '"dependencies": { \n',
+    var results = 'npm: \n "dependencies": { \n',
         errors = 'errors: \n',
+        bowers = 'bower: \n "dependencies": { \n',
         counter = 0;
     fs.readdir('./node_modules', function (err, dirs) {
         if (err) {
@@ -27,16 +28,54 @@ var fs = require('fs');
                             if (index + 1 !== dirs.length) {
                                 results += ', \n';
                             } else {
-                                results += '\n';
+                                results += '\n } \n \n \n ';
                             }
                         }
                     });
                 }
             }
         });
-    setTimeout(function () {
-        results = results + '} \n \n \n' + errors;
-        fs.writeFile('package.txt', results);
-    }, 1000);
+    });
+    fs.readdir('./public/lib', function (err, dirs) {
+        if (err) {
+            fs.writeFile('package.txt', err);
+            return;
+        }
+        dirs.forEach(function (dir, index) {
+            if (dir.indexOf('.') !== 0) {
+                var packageJsonFile = fs.existsSync('./public/lib/' + dir + '/package.json') ? './public/lib/' + dir + '/package.json' : './public/lib/' + dir + '/.bower.json';
+                if (fs.existsSync(packageJsonFile)) {
+                    fs.readFile(packageJsonFile, function (err, data) {
+                        if (err) {
+                            errors += err + ', \n';
+                        } else {
+                            var json = JSON.parse(data);
+                            bowers += '    "' + json.name + '": "' + json.version + '"';
+                            if (index + 1 !== dirs.length) {
+                                bowers += ', \n';
+                            } else {
+                                bowers += '} \n \n \n' + errors;
+                            }
+                        }
+                    });
+                } else {
+                    var fileSplit = packageJsonFile.split('/'),
+                        returnString = '';
+                    fileSplit = fileSplit.slice(0, -1);
+                    fileSplit.forEach(function (item, index) {
+                        if (index + 1 === fileSplit.length) {
+                            returnString += item + ', \n';
+                        } else {
+                            if (item !== undefined) returnString += item + '/';
+                        }
+                    });
+                    errors += returnString;
+                }
+            }
+        });
+        setTimeout(function () {
+            results = results + bowers;
+            fs.writeFile('package.txt', results);
+        }, 3000);
     });
 })();
